1c11ff20e6bad70b59f11318334755baf1131a9d
lezcanodev <102418001+lezcanodev@users.noreply.github.com>
2022-04-11 13:20:28 -1200

    Update index.php

--- index.php
+++ index.php
@@ -14,17 +14,9 @@ try{
     switch($e->getCode()){
         case 404: 
              header('HTTP/1.1 404 NOT FOUND');
-             echo json_encode([
-                'status' => '404',
-                'msg'   => 'NOT FOUND'
-            ]);
         break;
         case 403:
             header('HTTP/1.1 403 FORBIDEN');
-            echo json_encode([
-                'status' => '403',
-                'msg'   => 'FORBIDEN'
-            ]);
         break;
     }
 
